3c6d99730241eb32fa547021be573de6b11eef81
yamosan <yamorimasato@gmail.com>
2022-07-08 03:27:25 +0900

    delete posts and create users

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_19_184656) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "published"
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_165843) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "firebase_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
